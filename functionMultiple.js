7886216ca405c3000bc9899f34c2b97af537690f
delpilarsm <delpilarsm@gmail.com>
2020-01-29 11:12:03 -0600

    Add Function call from html index
    Function to create multiples of 100

--- functionMultiple.js
+++ functionMultiple.js
@@ -1,25 +1,40 @@
-functionMultiple = function(n) {
-	 value = ''+n;	
-	if (n == 1) return 'Linianos';		
-	switch (n%3){
-	  case 0: value = 'Linio';  break;
-	  case 1:
-	    switch(n%5){
-	      case 0:   
-		     value = 'IT'; break;
-	      case 1:
-  		     value = ''+n;    break;
-	    }
-		default: value= ''+n;  break;
-	}
-	switch (n%5) { 
-	   case 0: value = 'IT';  break;
-	}	
-	switch ((n%5 ==0) && (n%3==0)) {
-	   case true: 
-     	   console.log('Linianos');  break;
-	   case false: 	
-           console.log(value);  break;		   
-	}		
-	console.log (functionMultiple(n-1)); 
+alert('Activate the browser ´s JavaScript console, to see the numbers');
+function functionMultiple(n) {  
+    let value = '' + n;
+    const linio = 'Linio';
+    const linianos = 'Linianos';
+    const it = 'IT'
+    if (n == 1)
+        return linianos;
+    switch (n % 3) {
+        case 0:
+            value = linio;
+            break;
+        case 1:
+            switch (n % 5) {
+                case 0:
+                    value = it;
+                    break;
+                case 1:
+                    value = '' + n;
+                    break;
+            }
+        default:
+            value = '' + n;
+            break;
+    }
+    switch (n % 5) {
+        case 0:
+            value = it;
+            break;
+    }
+    switch ((n % 5 == 0) && (n % 3 == 0)) {
+        case true:
+            console.log(linianos);
+            break;
+        case false:
+            console.log(value);
+            break;
+    }    
+    console.log(functionMultiple(n - 1));
 }
